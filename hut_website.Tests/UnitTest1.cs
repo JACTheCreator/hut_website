e1732c905e8b2a201f739246f4cdaad5cf83152f
Jermaine Coates <jermainecoates@outlook.com>
2020-07-13 18:46:19 -0500

    Initial Website Commit

---
+++ hut_website.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace hut_website.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
